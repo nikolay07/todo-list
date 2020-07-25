28d4c7e5670ea3ad22883170d5e0bee4ea9a6c68
nikolay07 <nikolay0701@ukr.net>
2020-07-25 13:24:16 +0300

    build

---
+++ build/precache-manifest.3f931d1719149517edbf36b8a7b33185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f76d411295c866eb241c8fe4b842e6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93445d03e658aa1414ac",
+    "url": "/static/css/main.1fd93f3e.chunk.css"
+  },
+  {
+    "revision": "aef97812c649c890418d",
+    "url": "/static/js/2.8261ea84.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8261ea84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93445d03e658aa1414ac",
+    "url": "/static/js/main.cfac895e.chunk.js"
+  },
+  {
+    "revision": "bca2f208509ed9d2e542",
+    "url": "/static/js/runtime-main.dbd73d5b.js"
+  }
+]);
